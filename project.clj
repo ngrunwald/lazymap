f84aeceeb8ff761689f8f910b66c00f210d11e92
Nils Grunwald <nils@grunwald.fr>
2012-12-17 18:50:21 +0100

    update readme and project before release

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject fr.grunwald/lazymap "3.1.0-SNAPSHOT"
+(defproject fr.grunwald/lazymap "3.1.0"
   :description "Take the greediness out of maps"
   :url "https://github.com/ngrunwald/lazymap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :warn-on-reflection true)
